8620a43af5f97831c20a27083120c52b25871c7e
logan micheal hirst <loganmichealhirst@logans-MacBook-Pro.local>
2023-05-05 21:30:10 -0700

    changed some styles and added a background color

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-console.log('hello world')
+
 const startBtn = document.querySelector('.start-game-btn')
 const messageTag = document.querySelector('.message-tag')
 const numberInput = document.querySelector('.guessing-input')
 const form = document.querySelector('.guessing-form')
 const select = document.querySelector('#size')
+const gameSizeMessage = document.querySelector('.game-size-message')
 
 let number = undefined
 let multiplier = 100
 
 function startGame() {
+    gameSizeMessage.innerHTML = `Guess between 0 and ${multiplier}`
     form.classList.remove('display-none')
     number = Math.floor(Math.random() * multiplier)
-    console.log(number)
     messageTag.innerHTML = 'Guess a number'
 }
 
 function checkNumber(e) {
-    console.log(+numberInput.value === number)
-    console.log(+numberInput.value, number)
     e.preventDefault()
+    if (numberInput.value === '') return
     if (+numberInput.value === number) {
         messageTag.innerHTML = `YOU WIN! THE ANSWER WAS ${number}`
         form.classList.add('display-none')
@@ -32,7 +32,6 @@ function checkNumber(e) {
 
 function changeMultiplier(event) {
     multiplier = +event.target.value
-    console.log(event.target.value)
 }
 
 select.addEventListener('change', changeMultiplier)
